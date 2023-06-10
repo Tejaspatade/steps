6aa3787fec0a7d449d7765f287bb7148a36e7d52
Tejas Patade <tej.patade69@gmail.com>
2023-06-10 09:00:41 +0530

    Fully Completed

--- src/App.js
+++ src/App.js
@@ -1,3 +1,61 @@
+import React, { useState } from "react";
+
+const messages = [
+	"Learn React ⚛️",
+	"Apply for jobs 💼",
+	"Invest your new income 🤑",
+];
+
 export default function App() {
-	return <div className="steps">Hello</div>;
+	const [step, setStep] = useState(3);
+	const [isOpen, setIsOpen] = useState(true);
+
+	const handlePrevious = () => {
+		if (step > 1) setStep((s) => s - 1);
+	};
+	const handleNext = () => {
+		if (step < 3) setStep((s) => s + 1);
+	};
+
+	return (
+		<>
+			<button className="close" onClick={() => setIsOpen((o) => !o)}>
+				&times;
+			</button>
+			{isOpen && (
+				<div className="steps">
+					<div className="numbers">
+						<div className={step >= 1 ? "active" : ""}>1</div>
+						<div className={step >= 2 ? "active" : ""}>2</div>
+						<div className={step >= 3 ? "active" : ""}>3</div>
+					</div>
+
+					<p className="message">
+						Step {step}: {messages[step - 1]}
+					</p>
+
+					<div className="buttons">
+						<button
+							onClick={handlePrevious}
+							style={{
+								backgroundColor: "#7950f2",
+								color: "#fff",
+							}}
+						>
+							Previous
+						</button>
+						<button
+							onClick={handleNext}
+							style={{
+								backgroundColor: "#7950f2",
+								color: "#fff",
+							}}
+						>
+							Next
+						</button>
+					</div>
+				</div>
+			)}
+		</>
+	);
 }
